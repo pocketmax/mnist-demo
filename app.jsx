f37539b69e3ce8b19d74537cf7a2abcae2e2c63b
max <foo@bar.com>
2021-08-16 20:13:09 -0700

    refactored

--- app.jsx
+++ app.jsx
@@ -3,73 +3,91 @@ import ReactDOM from 'react-dom';
 import CanvasDraw from "react-canvas-draw";
 import * as tf from "@tensorflow/tfjs";
 
-class MyResults extends React.Component {
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.canvas = React.createRef();
+        this.state = {
+            predicted: null,
+            model: null
+        };
+
+        tf.loadLayersModel('/dm/model.json').then((model)=>{
+            this.setState({model});
+        });
+
+    }
+    componentDidMount() {
+    }
+
     render() {
-        const style = {
+
+        let predict = (input)=>{
+            this.state.model.predict([tf.tensor(input).reshape([1, 28, 28, 1])]).array().then((scores)=>{
+                scores = scores[0];
+                let predicted = scores.indexOf(Math.max(...scores));
+                this.setState({predicted});
+            });
+        }
+
+        let clearDrawing = ()=>{
+            console.log(this.canvas);
+            this.canvas.current.clear();
+            this.setState({predicted: ''});
+        };
+
+        let myOnChange = (e)=>{
+
+            console.log('myOnChange...');
+            let img = new Image();
+            img.onload = function() {
+                let context = e.canvas.drawing.getContext('2d');
+                context.drawImage(img, 0, 0, 28, 28);
+                let imgData = context.getImageData(0, 0, 28, 28);
+                let data = imgData.data;
+                let input = [];
+                for(let i = 0; i < data.length; i += 4) {
+                    input.push(data[i + 2] / 255);
+                }
+                console.log('input...');
+                predict(input);
+            };
+            img.src = e.canvas.drawing.toDataURL('image/png');
+        };
+
+        const canvasStyle = {
+            border: '1px solid red',
+            width: '200px',
+            height: '300px'
+        };
+        const resultsStyle = {
             border: '1px solid blue',
+            fontSize: '120px',
             width: '200px',
             height: '200px'
         };
-
-        return <div style={style}>
-            hello from results
+        const clearStyle = {
+            fontSize: '35px'
+        };
+        return <div className="App">
+            <CanvasDraw
+                style={canvasStyle}
+                ref={this.canvas}
+                onChange={myOnChange}
+                canvasWidth={200}
+                canvasHeight={300}
+            />
+            <div style={resultsStyle}>{this.state.predicted}</div>
+            <button
+                style={clearStyle}
+                onClick={clearDrawing}
+            >clear
+            </button>
         </div>;
     }
 }
 
-tf.loadLayersModel('/dm/model.json').then(function(model) {
-    window.model = model;
-    console.log('dm loaded!');
-});
-
-let myOnChange = (e)=>{
-
-    console.log('myOnChange...');
-    let img = new Image();
-    img.onload = function() {
-        let context = e.canvas.drawing.getContext('2d');
-        context.drawImage(img, 0, 0, 28, 28);
-        let imgData = context.getImageData(0, 0, 28, 28);
-        let data = imgData.data;
-        let input = [];
-        for(let i = 0; i < data.length; i += 4) {
-            input.push(data[i + 2] / 255);
-        }
-        console.log('input...');
-        predict(input);
-        console.log(input);
-    };
-    img.src = e.canvas.drawing.toDataURL('image/png');
-
-    // console.log(e.ctx.drawing.canvas.getImageData);
-};
-
-let predict = function(input) {
-    if (window.model) {
-        window.model.predict([tf.tensor(input).reshape([1, 28, 28, 1])]).array().then(function(scores){
-            scores = scores[0];
-            let predicted = scores.indexOf(Math.max(...scores));
-            // $('#number').html(predicted);
-            console.log(predicted);
-        });
-    } else {
-        // The model takes a bit to load, if we are too fast, wait
-        setTimeout(function(){predict(input)}, 50);
-    }
-}
-
-const App = () => (
-    <div className="App">
-        <h1 className="App-Title">Hello React</h1>
-        <CanvasDraw
-            onChange={myOnChange}
-            canvasWidth={200}
-            canvasHeight={300}
-        />
-        <MyResults></MyResults>
-        <button>clear</button>
-    </div>
-);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
